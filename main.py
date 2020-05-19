2c273153c1d58d81d8f884a3ca70c57afa5db356
Hamza Krika <63921551+Hemzyy@users.noreply.github.com>
2020-05-19 19:51:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import bs4, requests, sys, re
 from random import randint
+from instapy_cli import client
 
-goodlinks = []
+memelinks = []
 
-while goodlinks == []:
+while memelinks == []:
     res = requests.get('https://www.reddit.com/user/kerdaloo/m/dankmemer/')
     page = bs4.BeautifulSoup(res.text, 'html.parser')
 
@@ -13,11 +14,18 @@ while goodlinks == []:
     links = urlReg.findall(str(mo))
 
     for a in links:
-        goodlinks += [a.replace("amp;", "")]
+        memelinks += [a.replace("amp;", "")]
 
-
-meme = requests.get(goodlinks[randint(1, 2)].replace('"', ''))
-with open('./memes/meme.png', 'wb') as f:
+meme = requests.get(memelinks[randint(1, 2)].replace('"', ''))
+with open('./memes/meme.jpg', 'wb') as f:
     f.write(meme.content)
 
-print('Meme downloaded!')
+username = 'username'
+password = 'password'
+image = './file/path.jpg'
+text = 'This is a test.' + '\r\n' + '#test'
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+print('Meme posted!')
