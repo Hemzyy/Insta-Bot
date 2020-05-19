8038931fc1637ac48e642a9a741308fe18e7e213
Hamza Krika <63921551+Hemzyy@users.noreply.github.com>
2020-05-19 18:52:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ page = bs4.BeautifulSoup(res.text, 'html.parser')
 
 mo = [img for img in page.find_all('img', {'alt': 'Post image'})]
 
-urlReg = re.compile(r'https://i.redd.it/\w+.\w+')
+urlReg = re.compile(r'https://preview.redd.it/\w+.jpg?\S+')
 links = urlReg.findall(str(mo))
 
+goodlinks = []
+for a in links:
+    goodlinks += [a.replace("amp;", "")]
+
 if links != []:
-    meme = requests.get(links[randint(1, 2)])
+    meme = requests.get(goodlinks[randint(1, 2)].replace('"', ''))
 else:
     print('memes list is empty right now.')
     sys.exit()
